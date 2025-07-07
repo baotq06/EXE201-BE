acd2c6e537788c1d1465a1671da1568ba2dcab71
Chuowq11445 <truongtvhe173551@fpt.edu.vn>
2025-07-07 23:09:57 +0700

    Push code .server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://exe201-fe.vercel.app",
+    origin: "https://exe-201-fe-pink.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
